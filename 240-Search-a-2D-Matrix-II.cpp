f1064e2531380e252b04638dee587f9a79b9c542
Krish Kamlesh Jain <krish14@somaiya.edu>
2026-07-08 09:35:20 +0530

    [240] [Time Beats: 75.49%] [Memory Beats: 38.51%] - LeetPush

---
+++ 240-Search-a-2D-Matrix-II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=0;
+        int column=matrix[0].size()-1;
+        while(row<=matrix.size()-1 && column>=0){
+            if(matrix[row][column]==target){
+                return true;
+            }
+            else  if(matrix[row][column]<target){
+               row++;
+            }
+            else{
+                column--;
+            }
+        }
+        return false;
+    }
+};
