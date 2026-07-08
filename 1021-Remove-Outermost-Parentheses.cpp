74dbc1e94dafcf1031c71285c4a2c33dd21ecab5
Krish Kamlesh Jain <krish14@somaiya.edu>
2026-07-08 14:05:48 +0530

    Update 1021-Remove-Outermost-Parentheses.cppchanges

--- 1021-Remove-Outermost-Parentheses.cpp
+++ 1021-Remove-Outermost-Parentheses.cpp
@@ -1,3 +1,4 @@
+##method 1 using stack  time :-0(n) space(n)
 class Solution {
 public:
     string removeOuterParentheses(string s) {
@@ -7,6 +8,21 @@ public:
         if(s[i]==')')c.pop();
         if(!c.empty())ans.push_back(s[i]);
         if(s[i]=='(')c.push(s[i]);
+       
+      }
+      return ans;
+    }
+};
+## method 2 using counter time -0(n)  space(1)
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+      int counter=0;
+      string ans="";
+      for(int i=0;i<s.length();i++){
+        if(s[i]==')')counter--;
+        if(counter!=0)ans.push_back(s[i]);
+        if(s[i]=='(')counter++;
        
       }
       return ans;
