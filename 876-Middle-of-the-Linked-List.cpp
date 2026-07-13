ac0dc649f68411e3aea1d81c40e0f83e2d2cd19e
Krish Kamlesh Jain <krish14@somaiya.edu>
2026-07-13 20:57:58 +0530

    [876] [Time Beats: 100.00%] [Memory Beats: 26.66%] - LeetPush

---
+++ 876-Middle-of-the-Linked-List.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode*temp=head;
+        ListNode*jumper=head;
+        while(jumper!=nullptr && jumper->next!=nullptr){
+            temp=temp->next;
+            jumper=jumper->next->next;
+
+        }
+        return temp;
+    }
+};
